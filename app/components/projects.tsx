ead8c922cd9b6f85a81b4ae545a8eda4eb7ded7f
Shaan Kohli <SKohli01@fgfbrands.com>
2025-02-04 13:55:58 -0500

    added tech stack for projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,44 +1,68 @@
 import React from 'react';
+import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import paths from '../data/paths';
 
 const projects = [
     {
         name: 'StockFlow',
         description: 'Description for project one.',
-        githubLink: 'https://github.com/shaanstackz/StockFlow'
+        githubLink: 'https://github.com/shaanstackz/StockFlow',
+        techStack: ['JavaScript', 'Python', 'TypeScript', 'CSS']
     },
     {
         name: 'RoomMate',
         description: 'Description for project two.',
-        githubLink: 'https://github.com/shaanstackz/FGF-Hackathon'
+        githubLink: 'https://github.com/shaanstackz/FGF-Hackathon',
+        techStack: ['Python', 'HTML']
     },
     {
         name: 'Until Dawn Adventure',
         description: 'Description for project three.',
-        githubLink: 'https://github.com/shaanstackz/until-dawn-like-game'
+        githubLink: 'https://github.com/shaanstackz/until-dawn-like-game',
+        techStack: ['Python']
     },
     {
         name: 'War Card Game',
         description: 'Description for project four.',
-        githubLink: 'https://github.com/shaanstackz/War-Card'
+        githubLink: 'https://github.com/shaanstackz/War-Card',
+        techStack: ['Rust']
     },
     {
         name: 'MyTranslate',
         description: 'Description for project five.',
-        githubLink: 'https://github.com/shaanstackz/MyTranslate'
+        githubLink: 'https://github.com/shaanstackz/MyTranslate',
+        techStack: ['Python']
     },
     {
         name: 'A Healthy Lifestyle',
         description: 'Description for project six.',
-        githubLink: 'https://github.com/shaanstackz/A-Healthy-Lifestyle'
+        githubLink: 'https://github.com/shaanstackz/A-Healthy-Lifestyle',
+        techStack: ['HTML', 'CSS', 'JavaScript']
     },
     {
         name: 'Flight Reservation System',
         description: 'Description for project seven.',
-        githubLink: 'https://github.com/shaanstackz/Flight-Reservation-System'
+        githubLink: 'https://github.com/shaanstackz/Flight-Reservation-System',
+        techStack: ['Java']
     }
 ];
+
+const techIcons: { [key: string]: string } = {
+    'JavaScript': paths.javascript,
+    'Python': paths.python,
+    'TypeScript': paths.typescript,
+    'CSS': paths.css,
+    'HTML': paths.html,
+    'Rust': paths.rust,
+    'React': paths.react,
+    'Node.js': paths.nodejs,
+    'MongoDB': paths.mongodb,
+    'C#': paths.csharp,
+    'Java': paths.java,
+    'SQL': paths.sql,
+};
 
 const Projects: React.FC = () => {
     return (
@@ -50,8 +74,18 @@ const Projects: React.FC = () => {
                         <div key={index} className="bg-white p-6 rounded-lg shadow-md">
                             <h3 className="text-xl font-semibold mb-2">{project.name}</h3>
                             <p className="text-gray-700 mb-4">{project.description}</p>
+                            <div className="mb-4">
+                                <h4 className="font-semibold">Tech Stack:</h4>
+                                <ul className="list-none flex space-x-2">
+                                    {project.techStack.map((tech, techIndex) => (
+                                        <li key={techIndex} className="text-gray-600">
+                                            <Image src={techIcons[tech as keyof typeof techIcons]} alt={tech} width={24} height={24} title={tech} className='tech-icon'/>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
                             <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="text-gray-900 hover:text-gray-700">
-                                <FontAwesomeIcon icon={faGithub} className='md:w-6 md h-6 w-4 h-4' />
+                                <FontAwesomeIcon icon={faGithub} className="md:w-6 md h-6 w-4 h-4" />
                             </a>
                         </div>
                     ))}
--- app/data/paths.ts
+++ app/data/paths.ts
@@ -2,6 +2,23 @@ const paths = {
     resume: '',
     me: '/shaan.jpg',
     tmu: '/tmu.jpg',
+    angular: '/angular.png',
+    react: '/react.png',
+    nodejs: '/nodejs.png',
+    mongodb: '/mongodb.png',
+    bash: '/bash.png',
+    python: '/python.png',
+    java: '/java.png',
+    html: '/html.png',
+    css: '/css.png',
+    javascript: '/javascript.png',
+    rust: '/rust.png',
+    elixir: '/elixir.png',
+    dotnet: '/dotnet.png',
+    sql: '/sql.png',
+    vba: '/vba.png',
+    csharp: '/csharp.png',
+    typescript: '/typescript.png',
 }
  
 export default paths;
