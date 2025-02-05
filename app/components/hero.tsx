62de8f559c307944e8484531852cbe3bfe155b55
shaanstackz <shaan.kohli@torontomu.ca>
2025-02-04 23:50:13 -0500

    added fonts to hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,13 +7,13 @@ const Hero: React.FC = () => {
         <div className="bg-gray-900 text-white py-20 w-screen">
             <div className="container mx-auto px-6 text-center">
                 <Image src={paths.me} alt="Shaan" height={128} width={128} className="mx-auto mb-4 rounded-full object-cover" />
-                <h1 className="text-5xl font-bold mb-4 typing-effect mx-auto">Shaan Kohli</h1>
-                <div className="relative h-8 mb-8 disappearing-text flex justify-center items-center mx-auto">
-                    <span className="text-xl">Developer</span>
-                    <span className="text-xl">Software Engineer</span>
-                    <span className="text-xl">Tech Enthusiast</span>
-                    <span className="text-xl">Problem Solver</span>
-                    <span className="text-xl">Innovator</span>
+                <h1 className="text-5xl mb-4 typing-effect mx-auto font-fira-code">shaan kohli</h1>
+                <div className="relative h-8 mb-8 disappearing-text flex justify-center items-center mx-auto font-press-start-2p">
+                <span className="text-xl font-press-start-2p">developer</span>
+                    <span className="text-xl font-press-start-2p">software engineer</span>
+                    <span className="text-xl font-press-start-2p">tech enthusiast</span>
+                    <span className="text-xl font-press-start-2p">problem solver</span>
+                    <span className="text-xl font-press-start-2p">innovator</span>
                 </div>
                 <a href="#projects" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300">
                     View Projects
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
+import Head from 'next/head';
 
 
 export const metadata: Metadata = {
@@ -15,9 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`overflow-y-scroll overflow-x-hidden`}
-      >
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Fira+Code:wght@400;700&family=Press+Start+2P&display=swap" rel="stylesheet" />
+      </Head>
+      <body className={`overflow-y-scroll overflow-x-hidden font-roboto`}>
         {children}
       </body>
     </html>
