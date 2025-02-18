93302c09f673f81ae475954e373b8ba3b3710079
shaanstackz <shaan.kohli@torontomu.ca>
2025-02-18 17:22:58 -0500

    added certification

--- app/components/certifications.tsx
+++ app/components/certifications.tsx
@@ -42,19 +42,34 @@ const Certifications: React.FC = () => {
                 <h2 className="text-2xl font-bold mb-4 text-center transition duration-500 ease-in-out transform hover:scale-110">
                     Certifications
                 </h2>
-                <div className="bg-gray-200 text-black p-4 rounded shadow-lg border-t border-gray-200 pt-4 flex items-center transition duration-500 ease-in-out transform hover:scale-105 hover:bg-gray-300">
-                    {/* Use Next.js Image component for optimization */}
+                {/* Microsoft Power Platform Fundamentals */}
+                <div className="bg-gray-200 text-black p-4 rounded shadow-lg border-t border-gray-200 pt-4 flex items-center transition duration-500 ease-in-out transform hover:scale-105 hover:bg-gray-300 mb-4">
                     <Image
-                        src={paths.microsoft} // Make sure paths.microsoft is a valid path
-                        alt="Microsoft Certification"
-                        width={48}  // Set width of the image
-                        height={48} // Set height of the image
+                        src={paths.microsoft} 
+                        alt="Microsoft Power Platform Fundamentals"
+                        width={48}
+                        height={48}
                         className="object-contain mr-4 transition duration-500 ease-in-out transform hover:scale-110"
                     />
                     <div>
                         <h3 className="text-xl font-semibold">Microsoft Power Platform Fundamentals</h3>
                         <p className="text-gray-600">Issued by Microsoft</p>
                         <p className="text-gray-600">Date: January 2025</p>
+                    </div>
+                </div>
+                {/* Microsoft AI Fundamentals */}
+                <div className="bg-gray-200 text-black p-4 rounded shadow-lg border-t border-gray-200 pt-4 flex items-center transition duration-500 ease-in-out transform hover:scale-105 hover:bg-gray-300">
+                    <Image
+                        src={paths.microsoft} 
+                        alt="Microsoft AI Fundamentals"
+                        width={48}
+                        height={48}
+                        className="object-contain mr-4 transition duration-500 ease-in-out transform hover:scale-110"
+                    />
+                    <div>
+                        <h3 className="text-xl font-semibold">Microsoft Azure AI Fundamentals</h3>
+                        <p className="text-gray-600">Issued by Microsoft</p>
+                        <p className="text-gray-600">Date: February 2025</p> 
                     </div>
                 </div>
             </div>
